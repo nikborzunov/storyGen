15097161264352f1c9aec77e6fdca75aade1199b
Nikolai Borzunov <godofcode@Damnaptop.local>
2024-09-12 09:22:19 +0300

    Changed backend IP

--- src/services/StoryService.ts
+++ src/services/StoryService.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IHistory, IStory } from '../typing/story';
 import { addHistory, addStoriesToLibrary} from '../store/reducers/StorySlice';
 
-const API_URL = 'http://192.168.0.103:1001';
+const API_URL = 'http://192.168.0.102:1001';
 
 type IFetchStoryBody = {
 	themes: string[];
